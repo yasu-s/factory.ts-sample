d526c27255f7190edd14283c306b17234348575b
Yasuyuki Saito <ya_saito@uluru.jp>
2022-02-18 14:51:22 +0900

    ログ修正

--- src/sample1.ts
+++ src/sample1.ts
@@ -21,13 +21,13 @@ const sampleDataFactory = Factory.Sync.makeFactory<SampleData>({
 const main = () => {
   // 1件生成
   const obj = sampleDataFactory.build();
-  console.log(`id:${obj.id}, fistName:${obj.firstName}`);
+  console.log(`id:${obj.id}, fistName:${obj.firstName}, age:${obj.age}`);
 
   // 複数件生成
   const items = sampleDataFactory.buildList(2);
   console.log(`items.length:${items.length}`);
-  console.log(`items[0] - id:${items[0].id}, fistName:${items[0].firstName}`);
-  console.log(`items[1] - id:${items[1].id}, fistName:${items[1].firstName}`);
+  console.log(`items[0] - id:${items[0].id}, fistName:${items[0].firstName}, age:${items[0].age}`);
+  console.log(`items[1] - id:${items[1].id}, fistName:${items[1].firstName}, age:${items[1].age}`);
 };
 
 // メイン処理実行
