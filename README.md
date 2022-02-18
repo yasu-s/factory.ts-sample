d0ca346f231b8e6e39d4d8b7e0741101115dd45a
Yasuyuki Saito <ya_saito@uluru.jp>
2022-02-18 14:47:29 +0900

    サンプル実装

--- README.md
+++ README.md
@@ -7,5 +7,19 @@
 ## 動作環境
 
 - Node.js - 14.x
+- Yarn - 1.22.x
 - TypeScript - 4.5.x
 - [factory.ts](https://www.npmjs.com/package/factory.ts) - 0.5.x
+
+## 動作確認
+
+```bash
+# パッケージインストール
+yarn
+
+# TypeScriptビルド
+yarn build
+
+# サンプル1実行
+yarn start:1
+```
---
+++ src/sample1.ts
@@ -0,0 +1,29 @@
+import * as Factory from 'factory.ts';
+
+type SampleData = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
+const sampleDataFactory = Factory.Sync.makeFactory<SampleData>({
+  id: Factory.each((i) => i),
+  firstName: 'Bob',
+  lastName: 'Smith',
+  age: Factory.each((i) => 20 + (i % 10)),
+});
+
+const main = () => {
+  // 1件生成
+  const obj = sampleDataFactory.build();
+  console.log(`id:${obj.id}, fistName:${obj.firstName}`);
+
+  // 複数件生成
+  const items = sampleDataFactory.buildList(2);
+  console.log(`items.length:${items.length}`);
+  console.log(`items[0] - id:${items[0].id}, fistName:${items[0].firstName}`);
+  console.log(`items[1] - id:${items[1].id}, fistName:${items[1].firstName}`);
+};
+
+main();
